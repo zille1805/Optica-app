0347cd3774d08f1eb6be3a33311cf70e82ddfcf9
zille1805 <emmanuel.zille@gmail.com>
2021-08-25 01:36:50 -0300

    contador

--- src/componentes/ItemCount.jsx
+++ src/componentes/ItemCount.jsx
@@ -4,36 +4,33 @@ import { Link } from "react-router-dom"
 
 const ArrayItemcomp=[]
 
-export default function ItemConter({InitialValue, Stock}){
+export default function ItemConter({initialValue, stock}){
     
-    const [count, setCount]=useState(InitialValue)
-    const[stock, setStock]=useState(Stock)
-    const increment = ()=>{
-        if (Stock >= 1 ){
-            Item=1;
-            ArrayItemcomp.add(Item)
-            setCount(count-(-1))
-            setStock(stock-1)
+    const [count, setCount]=useState(initialValue)
+    const[Stock, setStock]=useState(stock)
+    
+    const Increment = ()=>{
+        if ( Stock >= 1 ){
+            setCount(count -(-1))
+            setStock(Stock - 1)
         }
     }
-    const decrement = ()=>{
-        if (count > 1 ){
-            ArrayItemcomp.splice(0, 1, 'Item')
-            setCount(count-1)
-            setStock(stock-(-1))
+    const Decrement = ()=>{
+        if (count >= 1 ){
+            // ArrayItemcomp.splice(0, 1, 'Item')
+            setCount(count - 1)
+            setStock(Stock -(-1))
         }
     }
     return(
         <div className="contador">
         <span>Cantidad de objetos: {Stock}, Cantidad a compara: {count}</span>
         <div>
-            <button  onClick={()=> increment()}>Sumar </button>
-            <button  onClick={()=> decrement()}>Restar</button>
+            <button  onClick={Increment}>Sumar </button>
+            <button  onClick={Decrement}>Restar</button>
             <button Text="Agregar al carrito" Variant="primary">Agregar al carrito </button>
             <div id="boton-terminar">
-                <Link to="/Cart">
-                    <ButtonBoostrap  Text="Terminar mi Compra" Variant="success"/> 
-                </Link>
+                {count > 0 && <Link to="/Cart"><ButtonBoostrap  Text="Terminar mi Compra" Variant="success"/></Link>}
             </div>
             
         </div>
--- src/componentes/ItemDetail.jsx
+++ src/componentes/ItemDetail.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
+import ItemConter from './ItemCount'
 
 export default function ItemDetail({titulo, Src, stock, precio,detail}) {
     
     return (
-        <>
-        <img src={Src} style={{ height: '400px', width: '450px' }} />
-        <h1>{titulo}</h1>
         <div>
-            <p>
-                {detail}
-            </p>
+            <img src={Src} style={{ height: '400px', width: '450px' }} />
+            <h1>{titulo}</h1>
+            <div>
+                <p>
+                    {detail}
+                </p>
+            </div>
+            <p>Precio: {precio}</p>
+            <ItemConter initialValue="1" stock={stock} />
+            
+
+            
         </div>
-        <p>Precio: {precio}</p>
-        </>
     )
 
 }
