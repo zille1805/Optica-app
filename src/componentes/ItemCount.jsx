a65e9f80a0467f9e6f40946d89969eac7a025394
zille1805 <emmanuel.zille@gmail.com>
2021-08-30 20:15:01 -0300

    botoncontador

--- src/componentes/ItemCount.jsx
+++ src/componentes/ItemCount.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react"
-import { useContext } from "react";
+
 import ButtonBoostrap from './Button.jsx'
 import { Link } from "react-router-dom"
-import {ThemeContext}  from "./contex/themecontext.jsx"
 const ArrayItemcomp=[]
 
 export default function ItemConter({initialValue, stockValue}){
     
     const [count, setCount]=useState(initialValue)
     const[Stock, setStock]=useState(stockValue)
-    const [ isOn, setIsOn] = useState(true)
+    const [ isOn, setIsOn] = useState(null)
     
     const Increment = ()=>{
         if ( Stock >= 1 ){
@@ -25,24 +24,25 @@ export default function ItemConter({initialValue, stockValue}){
         }
     }
     const Add=()=>{
-        if (count>1){
-            return(
-                setIsOn(!isOn)
-            )
-
+        if (count >= 1){
+            setIsOn(true)
         }
     }
     return(
         <div className="contador">
         <span>Cantidad de objetos: {Stock}, Cantidad a compara: {count}</span>
         <div>
-            <button  onClick={Increment}>Sumar </button>
-            <button  onClick={Decrement}>Restar</button>
-            <button Text="Agregar al carrito" Variant="primary" onClick={Add}>Agregar al carrito </button>
-            <div id="boton-terminar">
-                {count > 0 && <Link to="/Cart"><ButtonBoostrap  Text="Terminar mi Compra" Variant="success"/></Link>}
-            </div>
-            
+            {!isOn ?
+                <>
+                    <button  onClick={Increment}>Sumar </button>
+                    <button  onClick={Decrement}>Restar</button>
+                    <button Text="Agregar al carrito" Variant="primary" onClick={Add}>Agregar al carrito </button>
+                </>
+                :
+                    <div id="boton-terminar">
+                        <Link to="/Cart"><ButtonBoostrap  Text="Terminar mi Compra" Variant="success"/></Link>
+                    </div>  
+            }
         </div>
         </div>
     )
